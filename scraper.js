53c653a35f7653fba4d8f67ec0eb40ab377b09b0
runjac <156261137+runjac@users.noreply.github.com>
2025-06-14 14:51:03 -0500

    renovado

--- scraper.js
+++ scraper.js
@@ -12,7 +12,10 @@ app.post('/scrape', async (req, res) => {
     const { curp } = req.body;
 
     try {
-        const browser = await puppeteer.launch({ executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', headless: true });
+       const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
         await page.setExtraHTTPHeaders({
